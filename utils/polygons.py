c20c7100c3af19755b79fbe6934ea4081fdc5f6f
John Pazarzis <jpazarzis@gmail.com>
2024-02-11 22:22:07 -0500

    Completes missing docstrings.

--- utils/polygons.py
+++ utils/polygons.py
@@ -1,27 +1,42 @@
-"""Exposes a function that created the HTML to display a polygon."""
+"""Exposes a function that returns the HTMP code for containing rectangles."""
 
 import os
 import pathlib
 import uuid
 
-import geopandas as gpd
-import folium
 from geopy.geocoders import Nominatim
 from shapely.geometry import Point
-
-_HOME_DIR = pathlib.Path.home()
-_GEO_DB = os.path.join(_HOME_DIR, "resources", "polygons.gpkg")
+import folium
+import geopandas as gpd
 
 
 def make_map(post_code):
+    """Creates the HTML to display the conaining polygon for the post code.
+
+    :param str post_code: The post code to lookup.
+
+    :return: The HTML to display as a string.
+    """
     global _polygons
     if not _polygons:
         _polygons = _Polygons()
 
-    return _polygons.plot_polygon_for_postcode(post_code)
+    return _polygons._plot_polygon_for_postcode(post_code)
+
+
+###############################################################################
+#
+# What follows is private stuff that is not meant to be used from the outside.
+#
+###############################################################################
+
+# Aliases.
+_HOME_DIR = pathlib.Path.home()
+_GEO_DB = os.path.join(_HOME_DIR, "resources", "polygons.gpkg")
 
 
 class _Polygons:
+    """Keeps the multi-polygons database in memory."""
 
     def __init__(self, rows=None):
         """Initializer."""
@@ -31,7 +46,7 @@ class _Polygons:
             gdf = gpd.read_file(_GEO_DB, rows=rows)
         self.__gdf = gdf.to_crs(epsg=4326)
 
-    def get_containing_polygon(self, post_code):
+    def _get_containing_polygon(self, post_code):
         """Find the containing polygon for the passed in post_code.
 
         :param str post_code: The post code to lookup.
@@ -48,9 +63,17 @@ class _Polygons:
                 return polygon
         raise ValueError
 
-    def plot_polygon_for_postcode(self, post_code, fill_color=None):
+    def _plot_polygon_for_postcode(self, post_code, fill_color=None):
+        """For the passed in post_code returns the HTML for the polygon.
+
+        :param str post_code: The post code to lookup.
+
+        :param str fill_color: The color to use to fill the area (optional).
+
+        :returns: The HTML document for the containing polygon.
+        """
         fill_color = fill_color or 'green'
-        p = self.get_containing_polygon(post_code)
+        p = self._get_containing_polygon(post_code)
         centroid_longitude = p.centroid.x
         centroid_latitude = p.centroid.y
 
@@ -62,7 +85,7 @@ class _Polygons:
             p,
             style_function=lambda feature: {
                 'fillColor': fill_color,
-                'weight': 2  # Customize border weight
+                'weight': 2
             }
         )
 
@@ -75,6 +98,12 @@ class _Polygons:
 
     @classmethod
     def _make_point_from_postcode(cls, postcode):
+        """Retrieves the centroid for the passed in postcode.
+
+        :param str post_code: The post code to lookup.
+
+        :returns: Shapely.Point
+        """
         geolocator = Nominatim(user_agent="junkapp")
         location = geolocator.geocode(postcode)
         if location:
@@ -85,5 +114,6 @@ class _Polygons:
 _polygons = None
 
 if __name__ == '__main__':
+    # Self test..
     POST_CODE = "EC4N 8BH"
     print(make_map(POST_CODE))
